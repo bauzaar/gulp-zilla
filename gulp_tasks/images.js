de9d7c99d050c8445b4fc80713b55bf4abcd223f
Fabio <hi@kreo.me>
2015-04-03 01:19:51 +0200

    added error and size to images task

--- gulp_tasks/images.js
+++ gulp_tasks/images.js
@@ -3,5 +3,7 @@ var config = require('./_config'),
 
 $.gulp.task('images', function () {
     $.gulp.src(config.images.src + '*.{gif,jpg,png,svg}')
-        .pipe($.gulp.dest(config.images.dest));
+        .on('error', config.modules.errors)
+        .pipe($.gulp.dest(config.images.dest))
+        .pipe($.size({showFiles: true}));
 });
