69f57a1717b67005cc3f4ab3c9656cf72ae6d31f
Fabio <hi@kreo.me>
2015-04-15 17:12:00 +0200

    all ok

--- tasks/images.js
+++ tasks/images.js
@@ -3,7 +3,6 @@ var config = require('../lib/config'),
 
 $.gulp.task('images', function () {
     $.gulp.src(config.images['src'] + '*.{gif,jpg,png,svg}')
-        //.on('error', config.modules['errors'])
         .pipe($.gulp.dest(config.images['dest']))
         .pipe($.size({showFiles: true}));
 });
