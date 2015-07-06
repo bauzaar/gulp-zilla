0811115e0d66d2f6466c3650e0dc25ccadbd32c0
kreo <hi@kreo.me>
2015-07-06 17:23:46 +0200

    fix uglify

--- tasks/sub_tasks/mail.js
+++ tasks/sub_tasks/mail.js
@@ -2,12 +2,13 @@ var config = require('../../lib/config'),
     $ = config.plugins;
 
 $.gulp.task('mail:css', function () {
-    return gulp.src(config.mail['src'])
-        .pipe(inlineCss({
+    return $.gulp.src(config.mail['src'])
+        .pipe($.inline_css({
             applyStyleTags: true,
             applyLinkTags: true,
             removeStyleTags: true,
             removeLinkTags: true
         }))
-        .pipe(gulp.dest(config.mail['dest']));
+        .pipe($.gulp.dest(config.mail['dest']))
+        .pipe($.size({showFiles: true}));
 });
--- tasks/sub_tasks/scripts.js
+++ tasks/sub_tasks/scripts.js
@@ -6,7 +6,20 @@ $.gulp.task('js:build', function () {
         .bundle()
         .on('error', config.lib['errors'])
         .pipe($.source(config.browserify['output_name']))
-        .pipe($.streamify($.gulp_if(process.prod, $.uglify())))
+        .pipe($.streamify($.gulp_if(process.prod, $.uglify({
+            mangle: true,
+            compress: {
+                sequences: true,
+                dead_code: true,
+                conditionals: true,
+                booleans: true,
+                unused: true,
+                if_return: true,
+                join_vars: true,
+                drop_console: true
+            },
+            preserveComments: null
+        }))))
         .pipe($.gulp.dest(config.browserify['dest']))
         .pipe($.streamify($.size({showFiles: true})))
         .pipe($.gulp_if(!process.prod, $.livereload()))
