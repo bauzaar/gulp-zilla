a125a500812c3e12641cd91747e36b48d593cc14
kreo <hi@kreo.me>
2015-09-21 12:39:19 +0200

    added 3rd task

---
+++ tasks/sub_tasks/third_party_libs.js
@@ -0,0 +1,11 @@
+var config = require('../../lib/config'),
+    $ = config.plugins;
+
+$.gulp.task('3rd', function () {
+    return $.gulp.src(config.third_party_libs['src'] + '/**/*.js')
+        .pipe($.concat(config.third_party_libs['output_name'] + '.js'))
+        .pipe($.uglify({mangle:true}))
+        .pipe($.gulp.dest(config.third_party_libs['dest']))
+        .on('error', config.lib['errors'])
+        .pipe($.size({showFiles: true}));
+});
