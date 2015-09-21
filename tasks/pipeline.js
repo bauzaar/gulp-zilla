cf92a656fd8b3db4852f56431190c05ffcb21251
kreo <hi@kreo.me>
2015-09-21 17:01:54 +0200

    created build task

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -1,6 +1,25 @@
 var config = require('../lib/config'),
     $ = config.plugins;
 
+function build_scripts(){
+    $.gulp.task('build', function () {
+
+        var src_to_build = [
+            config.build['dest'] + '/**/' + config.build['vendor'] + '.js',
+            config.build['dest'] + '/**/' + config.build['app'] + '.js',
+            config.build['dest'] + '/**/' + config.build['libs'] + '.js'
+        ];
+
+        $.gulp.src(src_to_build)
+            .pipe($.concat('build.js'))
+            .pipe($.gulp.dest(config.build['dest']))
+            .on('error', config.lib['errors'])
+            .pipe($.size({showFiles: true}));
+
+        $.del(src_to_build);
+    });
+}
+
 $.gulp.task('ls', $.task_listing);
 
 $.gulp.task('install', function () {
@@ -25,9 +44,9 @@ $.gulp.task('mail', ['clean:mail_styles', 'clean:mail_templates'], function () {
 });
 
 $.gulp.task('serve', function () {
-    $.gulp.watch(config.serve['styles'], { interval: 900 }, ['clean:styles', 'sass:build']);
-    $.gulp.watch(config.serve['scripts'], { interval: 900 }, ['clean:scripts', 'js:build']);
-    $.gulp.watch(config.serve['markup'], { interval: 900 }, ['html:serve']);
-    $.gulp.watch(config.serve['mail'], { interval: 900 }, ['mail']);
-    $.gulp.watch(config.serve['bower'], { interval: 900 }, ['vendor']);
+    $.gulp.watch(config.serve['styles'], {interval: 900}, ['clean:styles', 'sass:build']);
+    $.gulp.watch(config.serve['scripts'], {interval: 900}, ['clean:scripts', 'js:build']);
+    $.gulp.watch(config.serve['markup'], {interval: 900}, ['html:serve']);
+    $.gulp.watch(config.serve['mail'], {interval: 900}, ['mail']);
+    $.gulp.watch(config.serve['bower'], {interval: 900}, ['vendor']);
 });
